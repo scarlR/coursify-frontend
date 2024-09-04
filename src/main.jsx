8ac9eb35fa7dea2974a11f720daf0c1258928f58
ScarlR <scarletraunak@gmail.com>
2024-09-04 22:02:43 +0530

    main.jsx updated

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { UserContextProvider } from './context/userContext.jsx'
-import { CourseContextProvider } from './context/CourseContext.jsx'
+import { UserContextProvider } from './context/userContext'
+import { CourseContextProvider } from './context/CourseContext'
 
 export const serverUrl = "https://coursify-backend-jn8l.onrender.com";
+
 createRoot(document.getElementById("root")).render(
     
         <UserContextProvider>
