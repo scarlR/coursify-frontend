a53c3db3a2bf1eb2384c911f84572aa7cce28a9f
ScarlR <scarletraunak@gmail.com>
2024-09-04 22:13:54 +0530

    fixed in build

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { UserContextProvider } from './context/userContext'
+UserContextProvider
 import { CourseContextProvider } from './context/CourseContext'
+import { UserContextProvider } from './context/UserContext'
+UserContextProvider
 
 export const serverUrl = "https://coursify-backend-jn8l.onrender.com";
 
